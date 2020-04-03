8c8a3b8fb3c50220a9b1b850ffb9760856115535
jueyoq <jueyoq@gmail.com>
2020-04-03 15:06:20 +0800

    init lad-2020

---
+++ src/mr/rpc.go
@@ -0,0 +1,20 @@
+package mr
+
+//
+// RPC definitions.
+//
+
+//
+// example to show how to declare the arguments
+// and reply for an RPC.
+//
+
+type ExampleArgs struct {
+	X int
+}
+
+type ExampleReply struct {
+	Y int
+}
+
+// Add your RPC definitions here.
